f4173aef4bdcf43ff5c655dfd6433e1cc14075db
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-08-19 11:07:50 +0530

    Add files via upload

---
+++ conditionalop.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class conditionalop {
+	public static void main(String args[])
+	{
+		int a=4;
+		String result=a<0?"negative":"positive";
+		System.out.println("the number is "+result);
+	}
+
+}
