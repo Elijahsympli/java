ba47e7e6ce98dda47e9d8bd884a3c6b2c69a733a
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-08-22 11:49:30 +0530

    Add files via upload

---
+++ armstrongwhile.java
@@ -0,0 +1,24 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class armstrongwhile {
+	public static void main(String arg[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a number");
+		int num=sc.nextInt();
+		int n=num;
+		int sum=0,rem;
+		while(num!=0)
+		{
+			rem=num%10;
+			sum=sum+rem*rem*rem;
+			num=num/10;
+		}
+		if(n==sum)
+		System.out.println(n+" is an armstrong number");
+		else
+			System.out.println(n+" is not an armstrong number");
+	}
+
+}
