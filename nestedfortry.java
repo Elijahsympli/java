5dfd07a55abd20ebc0384a8ec4d5b5e7090358f2
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-08-23 10:51:35 +0530

    Add files via upload

---
+++ nestedfortry.java
@@ -0,0 +1,17 @@
+package com.java;
+
+public class nestedfortry {
+	public static void main(String args[])
+	{
+		
+		for(int rows=5;rows>0;rows--)
+		{
+			for(int cols=1;cols<rows+1;cols++)
+			{
+				System.out.print("*"+" ");
+				
+			}
+			System.out.println();
+		}
+}
+}
