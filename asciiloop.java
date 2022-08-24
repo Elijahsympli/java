2cf6d25ce30e32e04809f3e172d782423fb9ca66
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-08-24 09:42:31 +0530

    Add files via upload

---
+++ asciiloop.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class asciiloop {
+	public static void main(String args[]) {
+		for (int j=65;j<=68;j++) {
+		 for(int i=65;i<=j;i++) {
+			char ch=(char)i;;
+			 System.out.print(ch+" ");
+		 } System.out.println("");
+	}
+	
+	}
+
+}
