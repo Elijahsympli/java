86169819d252fcca8452ff8a79d7c35f4b3a7a45
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-08-25 09:53:41 +0530

    Update rghtangtriwthno.java

--- rghtangtriwthno.java
+++ rghtangtriwthno.java
@@ -1,3 +1,16 @@
+/*Write a program in Java to display the pattern like right angle triangle with a number
+
+Test Data
+Input number of rows : 10
+Expected Output :
+
+1                                                                                
+12                                                                               
+123                                                                              
+1234                                                                             
+12345
+*/
+
 package com.java;
 
 public class rghtangtriwthno {
