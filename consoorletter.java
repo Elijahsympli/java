88aa3cba012f587a9b76ad92779f94c637d143d0
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-08-25 09:41:46 +0530

    Update consoorletter.java

--- consoorletter.java
+++ consoorletter.java
@@ -1,3 +1,6 @@
+/*Write a Java program that takes the user to provide a single character from the alphabet. Print Vowel or Consonant, depending on the user input. 
+If the user input is not a letter (between a and z or A and Z), print an error message*/
+
 package com.java;
 
 import java.util.Scanner;
