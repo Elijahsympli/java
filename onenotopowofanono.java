1ebca953c8b73678851bdcf154b745c70f2f4a9c
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-08-26 06:29:56 +0530

    Update onenotopowofanono.java

--- onenotopowofanono.java
+++ onenotopowofanono.java
@@ -1,3 +1,4 @@
+//Two numbers are entered through the keyboard. Write a program to find the value of one number raised to the power of another. (Do not use Java built-in method)
 package com.java;
 
 import java.util.Scanner;
