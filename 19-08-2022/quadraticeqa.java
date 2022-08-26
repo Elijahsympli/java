418de578bbbbf529fd77ac6fe825608c2ed28dc7
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-08-26 07:01:37 +0530

    Add files via upload

---
+++ 19-08-2022/quadraticeqa.java
@@ -0,0 +1,23 @@
+package com.java;
+
+public class quadraticeqa {
+
+
+	public static void main(String args[])
+	{
+	
+{
+	
+	int a=1,b=5,c=1;
+	double d=b*b-4*a*c;
+	if(d==0) {
+		System.out.println("roots are equal");
+	}else if(d>0) {
+		System.out.println("roots are real and diferent");
+	}else {
+		System.out.println("roots are imaginary");
+	}
+}
+	}
+}
+
