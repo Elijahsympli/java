4d1034cfdbade647554fd207247535671c76e9e0
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-08-26 07:49:54 +0530

    Add files via upload

---
+++ 25-08-2022/Area.java
@@ -0,0 +1,17 @@
+package assignment.java.pack;
+
+public class Area {
+
+	int num;
+	int num2;
+
+ public void setDim(int l,int b){
+	this.num=l;
+	this.num2=b;
+}
+public int getArea(){
+	return num*num2;
+}
+}
+
+
