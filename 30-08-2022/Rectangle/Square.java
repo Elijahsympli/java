fada891f88f9da3d39b43fa6ecec25f36ae7eda0
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-08-30 14:05:37 +0530

    Update Square.java

--- 30-08-2022/Rectangle/Square.java
+++ 30-08-2022/Rectangle/Square.java
@@ -1,14 +1,20 @@
 package com.java;
 
 public class Square extends Rectangle{
-
+int s;
 	Square(int sq,int l, int b) {
 		
 		super(sq,l, b);
 		super.Area();
 		super.Perimeter();
-		super.Sq();
+		
+		this.s=sq;
 	}
 	
-
+	void SqPerimeter() {
+		System.out.println("perimeter of Square="+4*s);
+	}
+	void SqArea() {
+		System.out.println("Area of Square="+s*s);
+	}
 }
