d67503730405f5ec923b2cc3356cbd37f53c72dd
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-09-01 12:49:33 +0530

    Add files via upload

---
+++ 01-09-2022/Inpstr3x3Arr.java
@@ -0,0 +1,23 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Inpstr3x3Arr {
+	public static void main(String args[]) {
+		
+		String a[][]=new String[3][3];
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++)
+		{Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		a[i][j]=str;
+		}
+		}
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++)
+		
+				System.out.println("a["+i+"]["+j+"]="+a[i][j]);
+		}
+		}
+		}
+
