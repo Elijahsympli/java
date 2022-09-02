8d2b0e3efa51a374f03494a9d630b4add9174fb2
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-09-02 14:09:59 +0530

    Update Q1.java

--- 02-09-2022/Q1.java
+++ 02-09-2022/Q1.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class Q1 {
 	public static void main(String args[]) {
 	int a[]=new int[5];
+	int k=0;
 	System.out.println("Enter 5 element of an array:");
 	Scanner sc=new Scanner(System.in);
 	for(int i=0;i<5;i++)
@@ -19,18 +20,28 @@ public class Q1 {
 	}
 	System.out.println("Enter a number to search it is present an array or not:");
 	int search=sc.nextInt();
-		System.out.println("the number entered  present in an array are:");
+
 	for(int i=0;i<5;i++)
 	{
 			if(a[i]==search) {
-				System.out.println("a["+i+"]="+search);
 				
-			}
-			
-				
-			
+				k=k+1;
+			}	
 	
 	}
-
+if (k==0) {
+	System.out.println("the entered number is not present in an array");
+}
+else {
+	System.out.println("the number entered  present in an array are:");
+	for(int i=0;i<5;i++)
+	{
+			if(a[i]==search) {
+				
+				System.out.println("a["+i+"]="+search);
+			}	
+	
+	}
+}
 }
 }
