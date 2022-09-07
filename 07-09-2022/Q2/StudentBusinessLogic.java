691ec918bd18229de4378922e65a63feaab7ebe2
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-09-07 13:32:46 +0530

    Add files via upload

---
+++ 07-09-2022/Q2/StudentBusinessLogic.java
@@ -0,0 +1,18 @@
+/*
+Create a class having student details like name,marks in 3 subjects and a percentage variable
+create a student business logic which should return the percentage of three subjects
+and then test the business logic using junit
+*/
+
+package com.Junit_Testing;
+
+public class StudentBusinessLogic {
+	public Object eg;
+
+	public double percentage(StudentDetails sd) {
+		
+		
+		return ((sd.getMarks1()+sd.getMarks2()+sd.getMarks3())*100)/sd.getMaxmarks();
+	}
+
+}
---
+++ 07-09-2022/Q2/StudentDetails.java
@@ -0,0 +1,46 @@
+
+package com.Junit_Testing;
+
+public class StudentDetails {
+private String name;
+private int marks1,marks2,marks3,maxmarks;
+
+public int getMaxmarks() {
+	return maxmarks;
+}
+public void setMaxmarks(int maxmarks) {
+	this.maxmarks = maxmarks;
+}
+public int getMarks1() {
+	return marks1;
+}
+public void setMarks1(int marks1) {
+	this.marks1 = marks1;
+}
+public int getMarks2() {
+	return marks2;
+}
+public void setMarks2(int marks2) {
+	this.marks2 = marks2;
+}
+public int getMarks3() {
+	return marks3;
+}
+public void setMarks3(int marks3) {
+	this.marks3 = marks3;
+}
+private double percent;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+
+public double getPercent() {
+	return percent;
+}
+public void setPercent(double percent) {
+	this.percent = percent;
+}
+}
