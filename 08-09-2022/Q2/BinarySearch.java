00a435f537902751305460454e11a20c917da30d
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-09-08 13:52:29 +0530

    Update BinarySearch.java

--- 08-09-2022/Q2/BinarySearch.java
+++ 08-09-2022/Q2/BinarySearch.java
@@ -5,14 +5,14 @@ class BinarySearch{
 	 
 	 public static void main(String args[]){  
 	        int arr[] = {1,2,3,4,5};  
-	        int key = 2,first=0;  
+	        int find = 5,first=0;  
 	        int last=arr.length-1;  
 	        
 	            int mid = (first + last)/2;  
 	   while( first <= last ){  
-	      if ( arr[mid] < key ){  
+	      if ( arr[mid] < find ){  
 	        first = mid + 1;     
-	      }else if ( arr[mid] == key ){  
+	      }else if ( arr[mid] ==find ){  
 	        System.out.println("Element is found at index: " + mid);  
 	        break;  
 	      }else{  
