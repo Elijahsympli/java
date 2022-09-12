f0bb59170013568e3ed4f28878a345aba15d4616
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-09-12 11:36:16 +0530

    Update ReverseString.java

--- 12-09-2022/Q1/ReverseString.java
+++ 12-09-2022/Q1/ReverseString.java
@@ -2,17 +2,20 @@
 //WAP to input a string and reverse it without using any inbuilt method
 package com.pack;
 
+import java.util.Scanner;
+
 public class ReverseString {
 public static void main(String args[]) {
-	
-	String s="home",k="";
+	Scanner sc=new Scanner(System.in);
+	String s=sc.next();
+	String k="";
 	int l=s.length()-1;
 	for(int i=0;i<s.length();i++)
 	{ k=k+s.charAt(l);
-		System.out.print(s.charAt(i));
+		
 		l--;
 	}
-	System.out.println();
+	
 	for(int i=0;i<s.length();i++)
 	{ 
 		System.out.print(k.charAt(i));
