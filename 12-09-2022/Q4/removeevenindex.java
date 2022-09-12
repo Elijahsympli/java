e62327a556b2e0dbe371aad0a31e1c2c61d426d4
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-09-12 13:24:19 +0530

    Update removeevenindex.java

--- 12-09-2022/Q4/removeevenindex.java
+++ 12-09-2022/Q4/removeevenindex.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class removeevenindex {
 public static void main(String args[]) {
 	
-;
+
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter a character present in  the given string to remove it  ");
 	String s=sc.next();
